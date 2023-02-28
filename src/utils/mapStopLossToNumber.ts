6d1769edb76b0fb4077298bd65a1b03ec84e1d5f
Viktors Ovcinnikovs <ovcinnikovs@verum.capital>
2023-02-28 18:28:32 +0200

    D8X-49 Calculate Order structures for StopLoss and TakeProfit

---
+++ src/utils/mapStopLossToNumber.ts
@@ -0,0 +1,14 @@
+import { StopLossE } from '../types/enums';
+
+const mapStopLoss: Record<StopLossE, number> = {
+  [StopLossE.None]: 0,
+  [StopLossE['10%']]: -0.1,
+  [StopLossE['25%']]: -0.25,
+  [StopLossE['50%']]: -0.5,
+  [StopLossE['75%']]: -0.75,
+  [StopLossE['90%']]: -0.9,
+};
+
+export function mapStopLossToNumber(stopLoss: StopLossE) {
+  return mapStopLoss[stopLoss];
+}
