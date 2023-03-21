4fa21ae8dfc9b169c4104afbe85844df0b11ad80
m66260 <m66260@protonmail.com>
2023-03-21 16:31:45 -0400

    if-else logic

--- src/blockchain-api/approveMarginToken.ts
+++ src/blockchain-api/approveMarginToken.ts
@@ -5,5 +5,13 @@ import { ERC20_ABI } from './constants';
 export function approveMarginToken(signer: ethers.providers.JsonRpcSigner, marginTokenAddr: string, proxyAddr: string) {
   const marginToken: ethers.Contract = new ethers.Contract(marginTokenAddr, ERC20_ABI, signer);
   const amount = BigNumber.from(2).pow(256).sub(BigNumber.from(1));
-  return marginToken.approve(proxyAddr, amount, { gasLimit: 1_000_000 });
+  return signer.getAddress().then((addr: string) => {
+    marginToken.allowance(addr, proxyAddr).then((allowance: BigNumber) => {
+      if(allowance.gt(0)) {
+        Promise.resolve();
+      } else {
+        marginToken.approve(proxyAddr, amount, { gasLimit: 1_000_000 });
+      }
+    })
+  });
 }
