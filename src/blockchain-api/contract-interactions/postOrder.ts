69b1a4f4e9b2fbb9165d336331a686097cbcf005
c40003141 <c40003141@protonmail.com>
2023-03-27 11:10:02 +0200

    fix formatting

--- src/blockchain-api/contract-interactions/postOrder.ts
+++ src/blockchain-api/contract-interactions/postOrder.ts
@@ -4,5 +4,5 @@ import { OrderDigestI } from 'types/types';
 
 export function postOrder(signer: ethers.Signer, signatures: string[], data: OrderDigestI) {
   const contract = new Contract(data.OrderBookAddr, LOB_ABI, signer);
-  return contract.postOrders(data.SCOrders, signatures, {gasLimit: 3_000_000});
+  return contract.postOrders(data.SCOrders, signatures, { gasLimit: 3_000_000 });
 }
