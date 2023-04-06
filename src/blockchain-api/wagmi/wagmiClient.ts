027cd5b32df1f2cf31adda180c08cab85d909f62
Viktors Ovcinnikovs <ovcinnikovs@verum.capital>
2023-04-06 12:12:49 +0300

    D8X-135 Upgrade wagmi and rainbowKit libs and add ZkEvm networks

--- src/blockchain-api/wagmi/wagmiClient.ts
+++ src/blockchain-api/wagmi/wagmiClient.ts
@@ -1,10 +1,13 @@
 import { connectorsForWallets } from '@rainbow-me/rainbowkit';
 import { coinbaseWallet, injectedWallet, metaMaskWallet, walletConnectWallet } from '@rainbow-me/rainbowkit/wallets';
 import { configureChains, createClient } from 'wagmi';
-import { polygonMumbai, polygon } from 'wagmi/chains';
+import { polygonMumbai, polygon, polygonZkEvm, polygonZkEvmTestnet } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 
-const { chains, provider } = configureChains([polygon, polygonMumbai], [publicProvider()]);
+const { chains, provider } = configureChains(
+  [polygon, polygonMumbai, polygonZkEvm, polygonZkEvmTestnet],
+  [publicProvider()]
+);
 
 const connectors = connectorsForWallets([
   {
