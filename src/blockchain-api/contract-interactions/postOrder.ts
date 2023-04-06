132b1ebc3783459e55e75c095264056d59a90e00
m66260 <m66260@protonmail.com>
2023-04-06 09:20:54 -0400

    lower gas estimate

--- src/blockchain-api/contract-interactions/postOrder.ts
+++ src/blockchain-api/contract-interactions/postOrder.ts
@@ -1,14 +1,10 @@
 import { Contract, ContractTransaction, BigNumber, Signer } from 'ethers';
-import { LOB_ABI } from 'blockchain-api/constants';
 import { OrderDigestI } from 'types/types';
 import { TraderInterface } from '@d8x/perpetuals-sdk';
 
-// upon redeployment of backend:
-// - LOB ABI should come from the BE
-
 export function postOrder(signer: Signer, signatures: string[], data: OrderDigestI): Promise<ContractTransaction> {
-  // const abi = typeof data.abi === 'string' ? [data.abi] : data.abi;
-  const contract = new Contract(data.OrderBookAddr, LOB_ABI, signer);
+  const abi = typeof data.abi === 'string' ? [data.abi] : data.abi;
+  const contract = new Contract(data.OrderBookAddr, abi, signer);
   const obOrders = TraderInterface.chainOrders(data.SCOrders, data.orderIds);
-  return contract.postOrders(obOrders, signatures, { gasLimit: BigNumber.from(3_000_000) });
+  return contract.postOrders(obOrders, signatures, { gasLimit: BigNumber.from(2_000_000) });
 }
