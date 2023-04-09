f35e75319ea528db5e8af56e3dd8bc8483a2e690
Viktors Ovcinnikovs <ovcinnikovs@verum.capital>
2023-04-09 16:08:21 +0300

    D8X-140 Change config-overrides.js file and add util lib

--- config-overrides.js
+++ config-overrides.js
@@ -3,13 +3,17 @@ const webpack = require('webpack');
 module.exports = function override(config) {
   const fallback = config.resolve.fallback || {};
   Object.assign(fallback, {
+    util: require.resolve('util/'),
     crypto: require.resolve('crypto-browserify'),
     stream: require.resolve('stream-browserify'),
+    buffer: require.resolve('buffer'),
     assert: require.resolve('assert'),
     http: require.resolve('stream-http'),
     https: require.resolve('https-browserify'),
     os: require.resolve('os-browserify'),
     url: require.resolve('url'),
+    fs: false,
+    path: false,
   });
 
   config.ignoreWarnings = [/Failed to parse source map/];
