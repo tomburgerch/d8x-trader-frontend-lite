7a257d25257a27ef1dd01ecf4ebf001138b8cae3
Viktors Ovcinnikovs <ovcinnikovs@verum.capital>
2023-04-11 11:33:18 +0300

    D8X-143 Implement rounding for Order size field

--- src/components/order-block/elements/order-size/OrderSize.tsx
+++ src/components/order-block/elements/order-size/OrderSize.tsx
@@ -1,18 +1,20 @@
+import { roundToLotString } from '@d8x/perpetuals-sdk';
 import { useAtom } from 'jotai';
 import type { ChangeEvent } from 'react';
-import { memo, useCallback } from 'react';
+import { memo, useCallback, useMemo } from 'react';
 
 import { Box, InputAdornment, OutlinedInput, Typography } from '@mui/material';
 
 import { InfoBlock } from 'components/info-block/InfoBlock';
 import { orderSizeAtom } from 'store/order-block.store';
-import { perpetualStatisticsAtom } from 'store/pools.store';
+import { perpetualStaticInfoAtom, perpetualStatisticsAtom } from 'store/pools.store';
 
 import styles from './OrderSize.module.scss';
 
 export const OrderSize = memo(() => {
   const [orderSize, setOrderSize] = useAtom(orderSizeAtom);
   const [perpetualStatistics] = useAtom(perpetualStatisticsAtom);
+  const [perpetualStaticInfo] = useAtom(perpetualStaticInfoAtom);
 
   const handleInputCapture = useCallback(
     (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -20,6 +22,19 @@ export const OrderSize = memo(() => {
     },
     [setOrderSize]
   );
+
+  const handleInputBlur = useCallback(() => {
+    if (perpetualStaticInfo) {
+      setOrderSize(+roundToLotString(orderSize, perpetualStaticInfo.lotSizeBC));
+    }
+  }, [perpetualStaticInfo, orderSize, setOrderSize]);
+
+  const orderSizeStep = useMemo(() => {
+    if (perpetualStaticInfo) {
+      return roundToLotString(perpetualStaticInfo.lotSizeBC, perpetualStaticInfo.lotSizeBC);
+    }
+    return 0.1;
+  }, [perpetualStaticInfo]);
 
   return (
     <Box className={styles.root}>
@@ -34,9 +49,10 @@ export const OrderSize = memo(() => {
           </InputAdornment>
         }
         type="number"
-        inputProps={{ step: 0.1, min: 0 }}
-        defaultValue={orderSize}
+        inputProps={{ step: orderSizeStep, min: 0 }}
+        value={orderSize}
         onChange={handleInputCapture}
+        onBlur={handleInputBlur}
       />
     </Box>
   );
