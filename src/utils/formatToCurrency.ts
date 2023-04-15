1b51786c5c2a84d726a4575eb4c092af915dc330
m66260 <m66260@protonmail.com>
2023-04-15 09:42:38 -0400

    fees in info block

--- src/utils/formatToCurrency.ts
+++ src/utils/formatToCurrency.ts
@@ -1,11 +1,14 @@
 const mapCurrencyToFractionDigits: Record<string, number> = {
   USD: 2,
-  MATIC: 2,
+  MATIC: 3,
   BTC: 5,
   ETH: 5,
 };
 
-export function formatToCurrency(value: number, currency = 'USD', fractionDigits?: number) {
+export function formatToCurrency(value: number | undefined, currency = 'USD', fractionDigits?: number) {
+  if (value === undefined) {
+    return '-';
+  }
   return `${new Intl.NumberFormat('en-US', {
     maximumFractionDigits: fractionDigits || mapCurrencyToFractionDigits[currency],
   }).format(value)} ${currency}`;
