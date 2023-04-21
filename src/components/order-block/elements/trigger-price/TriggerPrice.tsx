97049c4a72f5774767f51a5dcd4db8cbc84f9ca4
m66260 <m66260@protonmail.com>
2023-04-21 09:47:19 -0400

    use static info to render symbols

--- src/components/order-block/elements/trigger-price/TriggerPrice.tsx
+++ src/components/order-block/elements/trigger-price/TriggerPrice.tsx
@@ -4,7 +4,7 @@ import { ChangeEvent, memo, useCallback, useState } from 'react';
 import { Box, InputAdornment, OutlinedInput, Typography } from '@mui/material';
 
 import { orderTypeAtom, triggerPriceAtom } from 'store/order-block.store';
-import { perpetualStatisticsAtom } from 'store/pools.store';
+import { selectedPerpetualAtom } from 'store/pools.store';
 import { OrderTypeE } from 'types/enums';
 
 import styles from './TriggerPrice.module.scss';
@@ -13,7 +13,7 @@ import { InfoBlock } from '../../../info-block/InfoBlock';
 export const TriggerPrice = memo(() => {
   const [orderType] = useAtom(orderTypeAtom);
   const [triggerPrice, setTriggerPrice] = useAtom(triggerPriceAtom);
-  const [perpetualStatistics] = useAtom(perpetualStatisticsAtom);
+  const [selectedPerpetual] = useAtom(selectedPerpetualAtom);
 
   const [inputValue, setInputValue] = useState(`${triggerPrice}`);
 
@@ -59,7 +59,7 @@ export const TriggerPrice = memo(() => {
         id="trigger-size"
         endAdornment={
           <InputAdornment position="end">
-            <Typography variant="adornment">{perpetualStatistics?.quoteCurrency}</Typography>
+            <Typography variant="adornment">{selectedPerpetual?.quoteCurrency}</Typography>
           </InputAdornment>
         }
         inputProps={{ step: 1, min: 0 }}
