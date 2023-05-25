959e71eab02de03a4b531d85c10fe5de6e2eb2d4
Viktors Ovcinnikovs <ovcinnikovs@verum.capital>
2023-05-25 10:41:14 +0300

    D8XL-5 Improve layout by height dimension

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { memo } from 'react';
+import { useAtom } from 'jotai';
+import { memo, useEffect } from 'react';
+import { useResizeDetector } from 'react-resize-detector';
 import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
@@ -6,19 +8,30 @@ import { Box } from '@mui/material';
 
 import { StaticBackground } from 'components/static-background/StaticBackground';
 import { TraderPage } from 'pages/trader-page/TraderPage';
+import { appDimensionsAtom } from 'store/app.store';
 
 import '@rainbow-me/rainbowkit/styles.css';
 
 import styles from './App.module.scss';
 
 export const App = memo(() => {
+  const { width, height, ref } = useResizeDetector();
+
+  const [, setDimensions] = useAtom(appDimensionsAtom);
+
+  useEffect(() => {
+    setDimensions({ width, height });
+  }, [width, height, setDimensions]);
+
   return (
-    <Box className={styles.root}>
-      <StaticBackground />
-      <Routes>
-        <Route key="trader-page" path="*" element={<TraderPage />} />
-      </Routes>
-      <ToastContainer position="top-left" autoClose={10_000} />
+    <Box className={styles.root} ref={ref}>
+      <Box className={styles.content}>
+        <StaticBackground />
+        <Routes>
+          <Route key="trader-page" path="*" element={<TraderPage />} />
+        </Routes>
+        <ToastContainer position="top-left" autoClose={10_000} />
+      </Box>
     </Box>
   );
 });
---
+++ src/store/app.store.ts
@@ -0,0 +1,5 @@
+import { atom } from 'jotai';
+
+import { AppDimensionsI } from 'types/types';
+
+export const appDimensionsAtom = atom<AppDimensionsI>({});
