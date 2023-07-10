b7dbc192094fc6260ac509bcf93137be8d0970de
Maximilian Jonescu <jonescumaxi@gmail.com>
2023-07-10 17:14:28 +0200

    Add elements to ReferrerTab view

--- src/pages/refer-page/components/referrer-tab/ReferrerTab.tsx
+++ src/pages/refer-page/components/referrer-tab/ReferrerTab.tsx
@@ -1,5 +1,39 @@
+import { useAccount } from 'wagmi';
+import { useAtom } from 'jotai';
+
 import { Box } from '@mui/material';
 
+import { selectedPoolAtom } from 'store/pools.store';
+
+import { Overview, type OverviewItemI } from '../overview/Overview';
+import { Disclaimer } from '../disclaimer/Disclaimer';
+
+import styles from './ReferrerTab.module.scss';
+
+const disclaimerTextBlocks = [
+  'Earn rebates by inviting traders to trade on D8X. Rebates are airdropped to your wallet weekly.',
+  'Check out details on the D8X referral program.',
+];
+
 export const ReferrerTab = () => {
-  return <Box>Referrer View</Box>;
+  const [selectedPool] = useAtom(selectedPoolAtom);
+
+  const { address } = useAccount();
+  // TODO: MJO: Change hardcoded values
+  const overviewItems: OverviewItemI[] = [
+    {
+      title: 'Total referred trading volume',
+      value: address ? 99999 : '--',
+      poolSymbol: selectedPool?.poolSymbol ?? '--',
+    },
+    { title: 'Total earned rebates', value: address ? 23455 : '--', poolSymbol: selectedPool?.poolSymbol ?? '--' },
+  ];
+
+  return (
+    <Box className={styles.root}>
+      <Overview title="Your referrals" items={overviewItems} />
+      <Disclaimer title="Refer & Earn" textBlocks={disclaimerTextBlocks} />
+      <div className={styles.divider} />
+    </Box>
+  );
 };
