d87dca4b57237792ece8112472b153351b43b65e
m66260 <m66260@protonmail.com>
2023-07-19 09:59:33 -0400

    dynamic title

--- src/pages/trader-page/components/perpetual-stats/PerpetualStats.tsx
+++ src/pages/trader-page/components/perpetual-stats/PerpetualStats.tsx
@@ -1,5 +1,5 @@
 import { useAtom } from 'jotai';
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 
 import type { StatDataI } from 'components/stats-line/types';
 import { StatsLine } from 'components/stats-line/StatsLine';
@@ -8,6 +8,16 @@ import { formatToCurrency } from 'utils/formatToCurrency';
 
 export const PerpetualStats = () => {
   const [perpetualStatistics] = useAtom(perpetualStatisticsAtom);
+
+  useEffect(() => {
+    if (perpetualStatistics) {
+      document.title = formatToCurrency(
+        perpetualStatistics.midPrice,
+        `${perpetualStatistics.baseCurrency}-${perpetualStatistics.quoteCurrency} | D8X`,
+        true
+      );
+    }
+  }, [perpetualStatistics]);
 
   const items: StatDataI[] = useMemo(
     () => [
