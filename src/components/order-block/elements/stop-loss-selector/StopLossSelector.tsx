ffee423d22f18331ac21bd18e996746c5a260405
Viktors Ovcinnikovs <ovcinnikovs@verum.capital>
2023-07-21 20:46:16 +0300

    D8X-246 Implement StopLoss & TakeProfit custom price input

--- src/components/order-block/elements/stop-loss-selector/StopLossSelector.tsx
+++ src/components/order-block/elements/stop-loss-selector/StopLossSelector.tsx
@@ -1,38 +1,109 @@
 import classNames from 'classnames';
 import { useAtom } from 'jotai';
-import { memo } from 'react';
+import { ChangeEvent, memo, useCallback, useEffect, useMemo } from 'react';
 
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, InputAdornment, OutlinedInput, Typography } from '@mui/material';
 
 import { InfoBlock } from 'components/info-block/InfoBlock';
-import { stopLossAtom } from 'store/order-block.store';
-import { StopLossE } from 'types/enums';
+import { orderInfoAtom, stopLossAtom, stopLossPriceAtom } from 'store/order-block.store';
+import { selectedPerpetualAtom } from 'store/pools.store';
+import { OrderBlockE, StopLossE } from 'types/enums';
 
 import styles from './StopLossSelector.module.scss';
 
 export const StopLossSelector = memo(() => {
+  const [orderInfo] = useAtom(orderInfoAtom);
   const [stopLoss, setStopLoss] = useAtom(stopLossAtom);
+  const [, setStopLossPrice] = useAtom(stopLossPriceAtom);
+  const [selectedPerpetual] = useAtom(selectedPerpetualAtom);
+
+  useEffect(() => {
+    setStopLossPrice(null);
+  }, [orderInfo?.orderBlock, setStopLossPrice]);
+
+  const handleStopLossPriceChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      const stopLossPriceValue = event.target.value;
+      if (stopLossPriceValue !== '') {
+        setStopLossPrice(+stopLossPriceValue);
+        setStopLoss(null);
+      } else {
+        setStopLossPrice(null);
+        setStopLoss(StopLossE.None);
+      }
+    },
+    [setStopLossPrice, setStopLoss]
+  );
+
+  const handleStopLossChange = useCallback(
+    (stopLossValue: StopLossE) => {
+      setStopLossPrice(null);
+      setStopLoss(stopLossValue);
+    },
+    [setStopLossPrice, setStopLoss]
+  );
+
+  const minStopLossPrice = useMemo(() => {
+    if (orderInfo?.midPrice && orderInfo.orderBlock === OrderBlockE.Short) {
+      return orderInfo.midPrice;
+    }
+    return 0;
+  }, [orderInfo?.midPrice, orderInfo?.orderBlock]);
+
+  const maxStopLossPrice = useMemo(() => {
+    if (orderInfo?.midPrice && orderInfo.orderBlock === OrderBlockE.Long) {
+      return orderInfo.midPrice;
+    }
+    return undefined;
+  }, [orderInfo?.midPrice, orderInfo?.orderBlock]);
+
+  const stopLossValue = useMemo(() => {
+    if (orderInfo?.stopLossPrice != null) {
+      const minValue = Math.max(minStopLossPrice, orderInfo.stopLossPrice);
+      if (maxStopLossPrice) {
+        return Math.min(maxStopLossPrice, minValue);
+      }
+      return minValue;
+    }
+    return '';
+  }, [orderInfo?.stopLossPrice, minStopLossPrice, maxStopLossPrice]);
 
   return (
     <Box className={styles.root}>
-      <Box className={styles.label}>
-        <InfoBlock
-          title="Stop loss"
-          content={
-            <>
-              <Typography>You can specify a stop loss order to go along with your main order.</Typography>
-              <Typography>
-                If you select e.g. -50%, you create a second order, that will be triggered if your loss on your main
-                order reaches -50%.
-              </Typography>
-              <Typography>
-                Technically, you are specifying a stop-market order, of the opposing side (if your main order is a BUY
-                order, you are specifying a stop-market SELL order). The trigger price is automatically calculated such
-                that your overall loss is as per your selection, assuming you entered at the worst price your slippage
-                settings allow.
-              </Typography>
-            </>
+      <Box className={styles.labelHolder}>
+        <Box className={styles.label}>
+          <InfoBlock
+            title="Stop loss"
+            content={
+              <>
+                <Typography>You can specify a stop loss order to go along with your main order.</Typography>
+                <Typography>
+                  If you select e.g. -50%, you create a second order, that will be triggered if your loss on your main
+                  order reaches -50%.
+                </Typography>
+                <Typography>
+                  Technically, you are specifying a stop-market order, of the opposing side (if your main order is a BUY
+                  order, you are specifying a stop-market SELL order). The trigger price is automatically calculated
+                  such that your overall loss is as per your selection, assuming you entered at the worst price your
+                  slippage settings allow.
+                </Typography>
+              </>
+            }
+          />
+        </Box>
+        <OutlinedInput
+          id="custom-stop-loss-price"
+          className={styles.customPriceInput}
+          endAdornment={
+            <InputAdornment position="end">
+              <Typography variant="adornment">{selectedPerpetual?.quoteCurrency}</Typography>
+            </InputAdornment>
           }
+          type="number"
+          value={stopLossValue}
+          placeholder="-"
+          onChange={handleStopLossPriceChange}
+          inputProps={{ step: 0.01, min: minStopLossPrice, max: maxStopLossPrice }}
         />
       </Box>
       <Box className={styles.stopLossOptions}>
@@ -41,7 +112,7 @@ export const StopLossSelector = memo(() => {
             key={key}
             variant="outlined"
             className={classNames({ [styles.selected]: key === stopLoss })}
-            onClick={() => setStopLoss(key)}
+            onClick={() => handleStopLossChange(key)}
           >
             {key}
           </Button>
--- src/components/order-block/elements/take-profit-selector/TakeProfitSelector.tsx
+++ src/components/order-block/elements/take-profit-selector/TakeProfitSelector.tsx
@@ -1,38 +1,109 @@
 import classNames from 'classnames';
 import { useAtom } from 'jotai';
-import { memo } from 'react';
+import { ChangeEvent, memo, useCallback, useEffect, useMemo } from 'react';
 
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, InputAdornment, OutlinedInput, Typography } from '@mui/material';
 
 import { InfoBlock } from 'components/info-block/InfoBlock';
-import { takeProfitAtom } from 'store/order-block.store';
-import { TakeProfitE } from 'types/enums';
+import { orderInfoAtom, takeProfitAtom, takeProfitPriceAtom } from 'store/order-block.store';
+import { selectedPerpetualAtom } from 'store/pools.store';
+import { OrderBlockE, TakeProfitE } from 'types/enums';
 
 import styles from './TakeProfitSelector.module.scss';
 
 export const TakeProfitSelector = memo(() => {
+  const [orderInfo] = useAtom(orderInfoAtom);
   const [takeProfit, setTakeProfit] = useAtom(takeProfitAtom);
+  const [, setTakeProfitPrice] = useAtom(takeProfitPriceAtom);
+  const [selectedPerpetual] = useAtom(selectedPerpetualAtom);
+
+  const handleTakeProfitPriceChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      const takeProfitPriceValue = event.target.value;
+      if (takeProfitPriceValue !== '') {
+        setTakeProfitPrice(+takeProfitPriceValue);
+        setTakeProfit(null);
+      } else {
+        setTakeProfitPrice(null);
+        setTakeProfit(TakeProfitE.None);
+      }
+    },
+    [setTakeProfitPrice, setTakeProfit]
+  );
+
+  useEffect(() => {
+    setTakeProfitPrice(null);
+  }, [orderInfo?.orderBlock, setTakeProfitPrice]);
+
+  const handleTakeProfitChange = useCallback(
+    (takeProfitValue: TakeProfitE) => {
+      setTakeProfitPrice(null);
+      setTakeProfit(takeProfitValue);
+    },
+    [setTakeProfitPrice, setTakeProfit]
+  );
+
+  const minTakeProfitPrice = useMemo(() => {
+    if (orderInfo?.midPrice && orderInfo.orderBlock === OrderBlockE.Long) {
+      return orderInfo.midPrice;
+    }
+    return 0;
+  }, [orderInfo?.midPrice, orderInfo?.orderBlock]);
+
+  const maxTakeProfitPrice = useMemo(() => {
+    if (orderInfo?.midPrice && orderInfo.orderBlock === OrderBlockE.Short) {
+      return orderInfo.midPrice;
+    }
+    return undefined;
+  }, [orderInfo?.midPrice, orderInfo?.orderBlock]);
+
+  const takeProfitValue = useMemo(() => {
+    if (orderInfo?.takeProfitPrice != null) {
+      const minValue = Math.max(minTakeProfitPrice, orderInfo.takeProfitPrice);
+      if (maxTakeProfitPrice) {
+        return Math.min(maxTakeProfitPrice, minValue);
+      }
+      return minValue;
+    }
+    return '';
+  }, [orderInfo?.takeProfitPrice, minTakeProfitPrice, maxTakeProfitPrice]);
 
   return (
     <Box className={styles.root}>
-      <Box className={styles.label}>
-        <InfoBlock
-          title="Take profit"
-          content={
-            <>
-              <Typography>You can specify a take profit order to go along with your main order.</Typography>
-              <Typography>
-                If you select e.g., 100%, you create a second order, that will be triggered if your profit on your main
-                order reaches 100%.
-              </Typography>
-              <Typography>
-                Technically, you are specifying a limit order of the opposing side (if your main order is a BUY order,
-                you are specifying a limit SELL order). The limit price is automatically calculated such that your
-                overall profit is as per your selection, assuming you entered at the worst price your slippage settings
-                allow.
-              </Typography>
-            </>
+      <Box className={styles.labelHolder}>
+        <Box className={styles.label}>
+          <InfoBlock
+            title="Take profit"
+            content={
+              <>
+                <Typography>You can specify a take profit order to go along with your main order.</Typography>
+                <Typography>
+                  If you select e.g., 100%, you create a second order, that will be triggered if your profit on your
+                  main order reaches 100%.
+                </Typography>
+                <Typography>
+                  Technically, you are specifying a limit order of the opposing side (if your main order is a BUY order,
+                  you are specifying a limit SELL order). The limit price is automatically calculated such that your
+                  overall profit is as per your selection, assuming you entered at the worst price your slippage
+                  settings allow.
+                </Typography>
+              </>
+            }
+          />
+        </Box>
+        <OutlinedInput
+          id="custom-stop-loss-price"
+          className={styles.customPriceInput}
+          endAdornment={
+            <InputAdornment position="end">
+              <Typography variant="adornment">{selectedPerpetual?.quoteCurrency}</Typography>
+            </InputAdornment>
           }
+          type="number"
+          value={takeProfitValue}
+          placeholder="-"
+          onChange={handleTakeProfitPriceChange}
+          inputProps={{ step: 0.01, min: minTakeProfitPrice, max: maxTakeProfitPrice }}
         />
       </Box>
       <Box className={styles.takeProfitOptions}>
@@ -41,7 +112,7 @@ export const TakeProfitSelector = memo(() => {
             key={key}
             variant="outlined"
             className={classNames({ [styles.selected]: key === takeProfit })}
-            onClick={() => setTakeProfit(key)}
+            onClick={() => handleTakeProfitChange(key)}
           >
             {key}
           </Button>
