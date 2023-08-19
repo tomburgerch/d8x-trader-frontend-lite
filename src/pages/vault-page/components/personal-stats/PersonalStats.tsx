5762de8a86856f6ae011f58532b4c19f0ad628f7
c40003141 <117669273+c40003141@users.noreply.github.com>
2023-08-19 07:59:13 +0200

    fix typo (#189)

--- src/pages/vault-page/components/personal-stats/PersonalStats.tsx
+++ src/pages/vault-page/components/personal-stats/PersonalStats.tsx
@@ -83,7 +83,7 @@ export const PersonalStats = memo(({ withdrawOn }: PersonalStatsPropsI) => {
           />
         </Box>
         <Typography variant="bodyMedium" className={styles.statValue}>
-          {userAmount !== undefined ? formatToCurrency(userAmount, `d${selectedPool?.poolSymbol}, true`) : '--'}
+          {userAmount !== undefined ? formatToCurrency(userAmount, `d${selectedPool?.poolSymbol}`, true) : '--'}
         </Typography>
       </Box>
       <Box key="estimatedEarnings" className={styles.statContainer}>
