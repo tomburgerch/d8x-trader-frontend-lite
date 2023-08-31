6036838835c89b43da517c1c2e093a53a12a9217
Viktors Ovcinnikovs <ovcinnikovs@verum.capital>
2023-08-31 10:17:56 +0300

    D8XL-217 Hide "Order block" option for big screens

--- src/components/header/elements/settings-block/SettingsBlock.tsx
+++ src/components/header/elements/settings-block/SettingsBlock.tsx
@@ -2,7 +2,7 @@ import { useAtom } from 'jotai';
 import { type ChangeEvent, memo } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { Box, Switch, Typography } from '@mui/material';
+import { Box, Switch, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { Build } from '@mui/icons-material';
 
 import { enabledDarkModeAtom, orderBlockPositionAtom } from 'store/app.store';
@@ -12,6 +12,9 @@ import styles from './SettingsBlock.module.scss';
 
 export const SettingsBlock = memo(() => {
   const { t } = useTranslation();
+
+  const theme = useTheme();
+  const isBigScreen = useMediaQuery(theme.breakpoints.up('lg'));
 
   const [orderBlockPosition, setOrderBlockPosition] = useAtom(orderBlockPositionAtom);
   const [enabledDarkMode, setEnabledDarkMode] = useAtom(enabledDarkModeAtom);
@@ -32,14 +35,16 @@ export const SettingsBlock = memo(() => {
           {t('common.settings.ui-settings.title')}
         </Typography>
       </Box>
-      <Box className={styles.optionRow}>
-        <Typography variant="bodyMedium">{t('common.settings.ui-settings.order-block')}</Typography>
-        <Switch
-          className={styles.switch}
-          checked={orderBlockPosition === OrderBlockPositionE.Right}
-          onChange={handleOrderBlockChange}
-        />
-      </Box>
+      {isBigScreen && (
+        <Box className={styles.optionRow}>
+          <Typography variant="bodyMedium">{t('common.settings.ui-settings.order-block')}</Typography>
+          <Switch
+            className={styles.switch}
+            checked={orderBlockPosition === OrderBlockPositionE.Right}
+            onChange={handleOrderBlockChange}
+          />
+        </Box>
+      )}
       <Box className={styles.optionRow}>
         <Typography variant="bodyMedium">{t('common.settings.ui-settings.dark-mode')}</Typography>
         <Switch
