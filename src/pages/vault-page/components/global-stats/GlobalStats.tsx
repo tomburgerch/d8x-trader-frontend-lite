a09c4da0800111b7a244042899526ad351cafa89
SpasiboKojima <andrei.katsewich@yandex.ru>
2023-09-06 15:34:19 +0300

    Add mobile GlobalStats option

--- src/pages/vault-page/components/global-stats/GlobalStats.tsx
+++ src/pages/vault-page/components/global-stats/GlobalStats.tsx
@@ -10,9 +10,14 @@ import { formatToCurrency } from 'utils/formatToCurrency';
 import { dCurrencyPriceAtom, sdkConnectedAtom, triggerUserStatsUpdateAtom, tvlAtom } from 'store/vault-pools.store';
 import { selectedPoolAtom, traderAPIAtom } from 'store/pools.store';
 
+import styles from './GlobalStats.module.scss';
+import { useMediaQuery, useTheme } from '@mui/material';
+
 export const GlobalStats = () => {
   const { t } = useTranslation();
   const chainId = useChainId();
+  const theme = useTheme();
+  const isMobileScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
   const [selectedPool] = useAtom(selectedPoolAtom);
   const [traderAPI] = useAtom(traderAPIAtom);
@@ -67,31 +72,63 @@ export const GlobalStats = () => {
     return '--';
   }, [selectedPool?.poolSymbol, dCurrencyPrice, tvl]);
 
+  const weeklyAPY: StatDataI = useMemo(
+    () => ({
+      id: 'weeklyAPY',
+      label: t('pages.vault.global-stats.apy'),
+      value: weeklyAPI !== undefined ? formatToCurrency(weeklyAPI, '%', true, 2) : '--',
+      numberOnly: weeklyAPI !== undefined ? formatToCurrency(weeklyAPI, '', true, 2) : '--',
+      currencyOnly: weeklyAPI !== undefined ? '%' : '',
+    }),
+    [weeklyAPI, t]
+  );
+
   const items: StatDataI[] = useMemo(
     () => [
-      {
-        id: 'weeklyAPY',
-        label: t('pages.vault.global-stats.apy'),
-        value: weeklyAPI !== undefined ? formatToCurrency(weeklyAPI, '%', true, 2) : '--',
-      },
       {
         id: 'tvl',
         label: t('pages.vault.global-stats.tvl'),
         value: selectedPool && tvl != null ? formatToCurrency(tvl, selectedPool.poolSymbol, true) : '--',
+        numberOnly: tvl != null ? formatToCurrency(tvl, '', true) : '--',
+        currencyOnly: selectedPool && tvl != null ? selectedPool.poolSymbol : '',
       },
       {
         id: 'dSymbolPrice',
         label: t('pages.vault.global-stats.price', { poolSymbol: selectedPool?.poolSymbol }),
         value: dCurrencyPrice != null ? formatToCurrency(dCurrencyPrice, selectedPool?.poolSymbol, true) : '--',
+        numberOnly: dCurrencyPrice != null ? formatToCurrency(dCurrencyPrice, '', true) : '--',
+        currencyOnly: dCurrencyPrice != null ? selectedPool?.poolSymbol : '',
       },
       {
         id: 'dSymbolSupply',
         label: t('pages.vault.global-stats.supply', { poolSymbol: selectedPool?.poolSymbol }),
         value: dSupply,
+        numberOnly: dSupply,
       },
     ],
-    [weeklyAPI, selectedPool, tvl, dCurrencyPrice, dSupply, t]
+    [selectedPool, tvl, dCurrencyPrice, dSupply, t]
   );
+
+  if (isMobileScreen) {
+    return (
+      <div className={styles.statContainer}>
+        <div>
+          <div className={styles.statMainLabel}>{weeklyAPY.label}</div>
+          <span className={styles.statMainValue}>{weeklyAPY.numberOnly}</span>{' '}
+          <span className={styles.statValue}>{weeklyAPY.currencyOnly}</span>
+        </div>
+        <div className={styles.statsBlock}>
+          {items.map((item) => (
+            <div key={item.id}>
+              <div className={styles.statLabel}>{item.label}</div>
+              <span className={styles.statValue}>{item.numberOnly}</span>{' '}
+              <span className={styles.statCurrency}>{item.currencyOnly}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 
   return <StatsLine items={items} />;
 };
