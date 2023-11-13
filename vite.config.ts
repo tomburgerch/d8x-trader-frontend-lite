6724e4264639cb4ef45694063a2d69d63f4a2ef8
m66260 <117136064+m66260@users.noreply.github.com>
2023-11-13 11:27:06 -0500

    manifest.json -> manifest.webmanifest (#421)
    add headers in vite cfg

--- vite.config.ts
+++ vite.config.ts
@@ -40,6 +40,11 @@ export default defineConfig({
   ],
   server: {
     open: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+    },
   },
   optimizeDeps: {
     include: ['@d8x/perpetuals-sdk'],
