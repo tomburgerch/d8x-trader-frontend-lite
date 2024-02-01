15a8b89b5dc64c117110c870d5be3f3dcd556532
m66260 <117136064+m66260@users.noreply.github.com>
2024-02-01 10:45:54 -0500

    Cardona (#506)
    * cardona wagmi config
    
    * sdk version

--- src/utils/chains.ts
+++ src/utils/chains.ts
@@ -24,3 +24,28 @@ export const x1 = {
     },
   },
 } as const satisfies Chain;
+
+export const cardona = {
+  id: 2442,
+  name: 'Cardona',
+  network: 'cardona',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'ETH',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    public: { http: ['https://rpc.cardona.zkevm-rpc.com'] },
+    default: { http: ['https://rpc.cardona.zkevm-rpc.com'] },
+  },
+  blockExplorers: {
+    etherscan: { name: 'Cardona Explorer', url: 'https://explorer-ui.cardona.zkevm-rpc.com' },
+    default: { name: 'Cardona Explorer', url: 'https://explorer-ui.cardona.zkevm-rpc.com' },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 114091,
+    },
+  },
+} as const satisfies Chain;
