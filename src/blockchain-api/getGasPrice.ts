00c8cd1aa82e41d1088c67fa78024af2cc5a8e68
m66260 <117136064+m66260@users.noreply.github.com>
2024-03-26 12:18:38 -0400

    gas price buffer (#558)

--- src/blockchain-api/getGasPrice.ts
+++ src/blockchain-api/getGasPrice.ts
@@ -4,8 +4,11 @@ import { getGasPrice as getGasPriceWagmi } from '@wagmi/core';
 export async function getGasPrice(chainId?: number) {
   let gasPrice = await getGasPriceWagmi(wagmiConfig, { chainId });
   if (chainId === 195) {
-    // X1 gas prices are often stale -> add buffer
+    // X1 gas prices are often stale -> 50% buffer
     gasPrice = (gasPrice * 150n) / 100n;
+  } else {
+    // other chains should be okay -> 20% buffer
+    gasPrice = (gasPrice * 120n) / 100n;
   }
   return gasPrice;
 }
