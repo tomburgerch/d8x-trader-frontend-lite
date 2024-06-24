0ef8cb8addff7ae1cd2b01fa3c41f18df239e123
m66260 <m66260@protonmail.com>
2024-06-24 12:04:22 -0400

    obsolete logic

--- src/blockchain-api/generateStrategyAccount.ts
+++ src/blockchain-api/generateStrategyAccount.ts
@@ -1,6 +1,5 @@
 import { etc } from '@noble/secp256k1';
 import { type WalletClient, bytesToHex, stringToBytes } from 'viem';
-import secureLocalStorage from 'react-secure-storage';
 import { privateKeyToAccount } from 'viem/accounts';
 
 export async function generateStrategyAccount(walletClient: WalletClient) {
@@ -10,7 +9,5 @@ export async function generateStrategyAccount(walletClient: WalletClient) {
   const pk = await walletClient
     .signMessage({ message: 'Generate Strategy Account', account: walletClient.account })
     .then((sig) => bytesToHex(etc.hashToPrivateKey(stringToBytes(sig))));
-  const account = privateKeyToAccount(pk);
-  secureLocalStorage.setItem(`d8x_strategyAddress-${walletClient.account.address}`, account.address);
-  return account;
+  return privateKeyToAccount(pk);
 }
