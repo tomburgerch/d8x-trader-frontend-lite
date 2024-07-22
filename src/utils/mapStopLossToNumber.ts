00a06f07773d15064b70d865a992d724b5cdd26f
c40003141 <117669273+c40003141@users.noreply.github.com>
2024-07-22 09:31:42 +0200

    tpsl 5%

--- src/utils/mapStopLossToNumber.ts
+++ src/utils/mapStopLossToNumber.ts
@@ -2,7 +2,7 @@ import { StopLossE } from '../types/enums';
 
 const mapStopLoss: Record<StopLossE, number> = {
   [StopLossE.None]: 0,
-  [StopLossE['1%']]: -0.01,
+  [StopLossE['5%']]: -0.05,
   [StopLossE['25%']]: -0.25,
   [StopLossE['50%']]: -0.5,
   [StopLossE['75%']]: -0.75,
