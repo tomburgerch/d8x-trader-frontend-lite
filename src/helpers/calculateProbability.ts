836b3991ceb8f9de610ad604497cb63892f6adf5
c40003141 <117669273+c40003141@users.noreply.github.com>
2024-09-05 20:07:52 +0200

    prevent crash if negative prices

--- src/helpers/calculateProbability.ts
+++ src/helpers/calculateProbability.ts
@@ -1,6 +1,10 @@
 import { priceToProb } from '@d8x/perpetuals-sdk';
 
 export function calculateProbability(price: number, isNoVote: boolean) {
+  if (price < 0) {
+    return price;
+  }
+
   const probability = priceToProb(price);
   return isNoVote ? 1 - probability : probability;
 }
